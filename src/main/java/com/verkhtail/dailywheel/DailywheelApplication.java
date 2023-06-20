6fc08bc6f72455e4b9b05abcb8090f62bccd250b
dverkhov <denis.verkhovskii@t-systems.com>
2023-06-20 19:53:18 +0200

    Initial commit

---
+++ src/main/java/com/verkhtail/dailywheel/DailywheelApplication.java
@@ -0,0 +1,13 @@
+package com.verkhtail.dailywheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailywheelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailywheelApplication.class, args);
+	}
+
+}
